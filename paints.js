07f9bd83430f4953292277608d6f2b98bc8a6160
wild-shivam07 <74832140+wild-shivam07@users.noreply.github.com>
2022-01-13 22:18:53 +0530

    hello

--- paints.js
+++ paints.js
@@ -1,7 +1,9 @@
 function Menu(){
 	document.getElementById("menu").classList.toggle("icon");
-	document.getElementById("nav").classList.toggle("change"); 
+	document.getElementById("nav").classList.toggle("change");
+	document.getElementById("drop").style.display = "block";
 }
 function drop(){
+	document.getElementById("drop").classList.toggle("hola");
 	setTimeout(Menu,100)
 }
